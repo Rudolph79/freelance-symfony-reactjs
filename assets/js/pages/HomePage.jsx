9a0928eaa393a818f7c16b699057fa4b394ec5c5
EBANG MEZUI ME MVE <rudolph.ebang@gmail.com>
2020-04-22 20:22:57 +0200

    Update homepage et Fix: Erreur sur le chrono lors de la création d'une première facture

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const HomePage = (props) => {
     return (
         <div className="jumbotron">
-            <h1 className="display-3">Hello, world!</h1>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
-                attention to featured content or information.</p>
+            <h1 className="display-3">Bonjour, les Freelances</h1>
+            <p className="lead">
+                Il s'agit d'une simple application pour les personnes en Freelances
+                afin de gérer leurs facturations. En tant qu'utilisateur vous pouvez créer votre compte,
+                créer des clients et leur attribuer à chacun une ou plusieurs factures.<br />
+                Pour chacune de ces factures vous pouvez la mettre à un état: envoyée, annulée, payée.
+            </p>
             <hr className="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger
-                    container.</p>
+                <p>
+                    <strong>NB</strong>: Cette application n'est pas professionnelle elle est mise en ligne
+                    juste pour des tests. C'est une base de données de faible capacité.<br />
+                    Merci
+                </p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <Link to="/register" className="btn btn-primary btn-lg">
+                        Inscrivez-vous !
+                    </Link>
                 </p>
         </div>
     );
